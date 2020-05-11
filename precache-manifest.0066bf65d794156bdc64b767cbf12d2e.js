798afea755f25f9ef3dc82cd87d833690983f8ca
Ashwani Gupta <Ashhwanigupta1@gmail.com>
2020-05-11 20:55:26 +0530

    Updates

---
+++ precache-manifest.0066bf65d794156bdc64b767cbf12d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bfed6064985d61989eeebdea2dcdae",
+    "url": "/registration-form/index.html"
+  },
+  {
+    "revision": "e3423f689b9f0cf379f1",
+    "url": "/registration-form/static/css/main.a9aa37ea.chunk.css"
+  },
+  {
+    "revision": "1e320649cc89dd6bbfc6",
+    "url": "/registration-form/static/js/2.375ad072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration-form/static/js/2.375ad072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3423f689b9f0cf379f1",
+    "url": "/registration-form/static/js/main.50a2e6d1.chunk.js"
+  },
+  {
+    "revision": "c3ac306fd03ca0d3b8ca",
+    "url": "/registration-form/static/js/runtime-main.6e8d7a18.js"
+  },
+  {
+    "revision": "c88cecbffad6d8e731fd95de49561ebd",
+    "url": "/registration-form/static/media/Montserrat-SemiBold.c88cecbf.ttf"
+  }
+]);
